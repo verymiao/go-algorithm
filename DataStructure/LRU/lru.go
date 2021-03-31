03e122287fcea758b34eb82693472316eaf42d72
verymiao <lkqdmsk@gmail.com>
2021-04-01 02:48:30 +0800

    lru cache

---
+++ DataStructure/LRU/lru.go
@@ -0,0 +1,81 @@
+package LRU
+
+//DlinkedNode
+type DlinkedNode struct {
+	key, value int
+	prev, next *DlinkedNode
+}
+
+func initDlinkedNode(key int, value int) *DlinkedNode {
+	return &DlinkedNode{key: key, value: value}
+}
+
+type LRUCache struct {
+	size       int
+	capacity   int
+	cache      map[int]*DlinkedNode
+	head, tail *DlinkedNode
+}
+
+func Constructor(capacity int) LRUCache {
+	l := LRUCache{
+		cache:    map[int]*DlinkedNode{},
+		head:     initDlinkedNode(0, 0),
+		tail:     initDlinkedNode(0, 0),
+		capacity: capacity,
+	}
+	l.head.next = l.tail
+	l.tail.prev = l.head
+	return l
+}
+
+func (this *LRUCache) Get(key int) int {
+	if _, ok := this.cache[key]; !ok {
+		return -1
+	}
+	node := this.cache[key]
+	this.moveToHead(node)
+	return node.value
+}
+
+func (this *LRUCache) Put(key int, value int) {
+	if _, ok := this.cache[key]; !ok {
+		node := initDlinkedNode(key, value)
+		this.cache[key] = node
+		this.addToHead(node)
+		this.size++
+		if this.size > this.capacity {
+			removed := this.removeTail()
+			delete(this.cache, removed.key)
+			this.size--
+		}
+	} else {
+		node := this.cache[key]
+		node.value = value
+		this.moveToHead(node)
+	}
+
+}
+
+func (this *LRUCache) addToHead(node *DlinkedNode) {
+	node.prev = this.head
+	node.next = this.head.next
+	this.head.next.prev = node
+	this.head.next = node
+}
+
+func (this *LRUCache) removeNode(node *DlinkedNode) {
+	node.prev.next = node.next
+	node.next.prev = node.prev
+}
+
+func (this *LRUCache) moveToHead(node *DlinkedNode) {
+	this.removeNode(node)
+	this.addToHead(node)
+}
+
+func (this *LRUCache) removeTail() *DlinkedNode {
+	node := this.tail.prev
+	this.removeNode(node)
+	return node
+}
---
+++ DataStructure/LRU/lru_test.go
@@ -0,0 +1,28 @@
+package Lru
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestLRUCache_Put(t *testing.T) {
+	lru := Constructor(2)
+	lru.Put(1, 1)
+	lru.Put(2, 2)
+	lru.Put(3, 3)
+	tw1 := lru.Get(2)
+	lru.Put(2, 4)
+
+	one1 := lru.Get(1)
+
+	if tw1 != 2 {
+		t.Logf("lru.Get(2) shoud be 2, but get: %v\n", tw1)
+		t.Fail()
+	}
+	//fmt.Println(one1)
+
+	if !reflect.DeepEqual(one1, -1) {
+		t.Logf("lru.Get(1) shoud be -1, but get: %v\n", one1)
+		t.Fail()
+	}
+}
